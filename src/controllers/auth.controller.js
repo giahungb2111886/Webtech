448ad5dcb6a89050c4a490adf134f1db499183ad
thaiphamse <pqthai2909@gmail.com>
2024-11-15 20:49:31 +0700

    fix: Jsend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,35 @@
 const authService = require("../services/auth.service.js");
 const ApiError = require("../api-error");
-const bcrypt = require('bcrypt');
 const JSend = require("../jsend");
 
 // Đăng ký
 const register = async (req, res) => {
-    try {
-        const rs = await authService.register(req.body)
-        res.status(201).json(JSend({ rs }));
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: 'Lỗi server', error });
-    }
+  try {
+    const rs = await authService.register(req.body);
+    res.status(201).json(JSend.success({ rs }));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Lỗi server", error });
+  }
 };
 
 // Đăng nhập
 const login = async (req, res) => {
-    try {
-        const rs = await authService.login(req.body)
+  try {
+    const rs = await authService.login(req.body);
 
-        res.status(200).json(JSend({ rs }));
-    } catch (error) {
-        res.status(500).json({ message: 'Lỗi server', error });
-    }
+    res.status(200).json(JSend.success({ rs }));
+  } catch (error) {
+    res.status(500).json({ message: "Lỗi server", error });
+  }
 };
 
 const logout = (req, res) => {
-    res.status(200).json(JSend({ rs }));
+  res.status(200).json(JSend.success({ rs }));
 };
 
 module.exports = {
-    register,
-    login,
-    logout
-}
+  register,
+  login,
+  logout,
+};
